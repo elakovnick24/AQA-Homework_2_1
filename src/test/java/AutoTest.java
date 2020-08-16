02ff582434a0e9046fcb724429e77d17730b9ee4
Nikita Elakov <89816960858@mail.ru>
2020-08-17 01:45:43 +0300

    Create SUT

---
+++ src/test/java/AutoTest.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class AutoTest {
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:7777");
+        SelenideElement form = $("[method=post]");
+        form.$("[data-test-id=name] input").setValue("Василий Алибабаевич");
+        form.$("[data-test-id=phone] input").setValue("+79778889999");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+
+        $("[data-test-id]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
+
+
