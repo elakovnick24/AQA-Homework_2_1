337baa2f22c3f72f8c515d529b261db37d4a671e
Nikita Elakov <89816960858@mail.ru>
2020-08-19 02:07:11 +0300

    added two test for validation name and phone

--- src/test/java/AutoTest.java
+++ src/test/java/AutoTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -14,32 +16,27 @@ public class AutoTest {
         $("[type=button]").click();
         $("[data-test-id]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
-}
 
-/*    @Test
+    @Test
     void validationCheckName() {
         open("http://localhost:7777");
-        SelenideElement form = $("[method=post]");
-        form.$("[data-test-id=name] input").setValue("Marusya");
-        form.$("[data-test-id=phone] input").setValue("+79778889999");
-        form.$("[data-test-id=agreement]").click();
-        form.$("[type=button]").click();
-
-        $("[data-test-id]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+       $("[data-test-id=name] input").setValue("Marusya");
+       $("[data-test-id=phone] input").setValue("+79778889999");
+       $("[data-test-id=agreement]").click();
+       $("[type=button]").click();
+       $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void validationCheckPhone() {
         open("http://localhost:7777");
-        SelenideElement form = $("[method=post]");
-        form.$("[data-test-id=name] input").setValue("Marusya");
-        form.$("[data-test-id=phone] input").setValue("+797788899990000");
-        form.$("[data-test-id=agreement]").click();
-        form.$("[type=button]").click();
-
-        $("[data-test-id]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=name] input").setValue("Никита Несборочный");
+        $("[data-test-id=phone] input").setValue("+797788899990000");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
-}*/
+}
 
 
 
