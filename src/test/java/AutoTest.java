b11493bace8161ea22f1481de11de2f835675d36
Nikita Elakov <89816960858@mail.ru>
2020-08-18 10:44:24 +0300

    add test for  validation

--- src/test/java/AutoTest.java
+++ src/test/java/AutoTest.java
@@ -17,6 +17,31 @@ public class AutoTest {
 
         $("[data-test-id]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+
+    @Test
+    void validationCheckName() {
+        open("http://localhost:7777");
+        SelenideElement form = $("[method=post]");
+        form.$("[data-test-id=name] input").setValue("Marusya");
+        form.$("[data-test-id=phone] input").setValue("+79778889999");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+
+        $("[data-test-id]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void validationCheckPhone() {
+        open("http://localhost:7777");
+        SelenideElement form = $("[method=post]");
+        form.$("[data-test-id=name] input").setValue("Marusya");
+        form.$("[data-test-id=phone] input").setValue("+797788899990000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+
+        $("[data-test-id]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
 }
+
 
 
